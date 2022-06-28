f155ba9510c45a00a9daec80177238d27503326a
kmiqbal19 <km.iqbal01@gmail.com>
2022-06-29 01:19:47 +0200

    before deploy

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 const ConnectDB = require("./config/db");
+// GLOBAL MIDDLEWARE IMPORT
 const errorMiddleware = require("./middlewares/errorMiddleware");
 
 // Import Routers
@@ -22,14 +27,30 @@ const app = express();
 // .ENV CONFIG
 dotenv.config();
 // CORS
-app.use(cors());
+app.use("*", cors());
 // APP STATIC FILE PATH CONFIG
 app.use(express.static(path.join(__dirname, "public/img")));
-// CONNECT TO DB
+// CONNECT TO DATABASE
 ConnectDB();
 // BODY PARSER AND URL ENCODED PARSER
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: false }));
+// Set security HTTP headers
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
+// Data sanitization against NOSQL query Injection
+app.use(mongoSanitize());
+// Data sanitization
+app.use(xss());
+// Preventing parameter pollution
+app.use(
+  hpp({
+    // whitelist: "duration",
+  })
+);
 // USE MORGAN IN DEVELOPMENT MODE ONLY
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -37,12 +58,13 @@ if (process.env.NODE_ENV === "development") {
 // REST API
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tasks", taskRouter);
+// USE GLOBAL ERROR HANDLER
+app.use(errorMiddleware);
 // BASIC APP GET MIDDELWARE FOR ('/')
 app.get("/", (req, res, next) => {
   res.send("Backend is running!");
 });
-// USE GLOBAL ERROR HANDLER
-app.use(errorMiddleware);
+
 // CREATE SERVER AND LISTEN TO SERVER
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
