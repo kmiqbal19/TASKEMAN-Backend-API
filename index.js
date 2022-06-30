cb5c5971ab24b2d1f122b38719e7006890c6f0f6
kmiqbal19 <km.iqbal01@gmail.com>
2022-06-30 20:16:07 +0200

    added cloudinary for backend file storage

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ app.use(express.static(path.join(__dirname, "public/img")));
 // CONNECT TO DATABASE
 ConnectDB();
 // BODY PARSER AND URL ENCODED PARSER
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 // Set security HTTP headers
 app.use(
   helmet({
